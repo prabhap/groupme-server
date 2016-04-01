752d80e6ad46445b21b75c2692fd5033ff90992b
Prabha Pattabiraman <prabhap@thoughtworks.com>
2016-04-01 12:28:58 +0530

    Prabha - Creating user only for the first time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def create
-    user = User.create(phone_number: params[:phoneNumber])
+    user = User.find_or_create_by(phone_number: params[:phoneNumber])
     render text: user.id and return
   end
 end
