67fad3b5c91423bc29c8298e864377757ef0ceac
Prabha Pattabiraman <prabhap@thoughtworks.com>
2016-04-04 10:50:53 +0530

    Prabha - showing only unsubscribed groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,7 @@ class GroupsController < ApplicationController
     @latitude, @longitude = *params[:l].split(',')
     # @groups = Group.where(:latitude => @latitude, :longitude => @longitude)
     # @groups = Group.all.select(:name)
-    @groups = Group.near([@latitude, @longitude], 1, :units => :km).where.not(@user.groups)
+    @groups = Group.near([@latitude, @longitude], 1, :units => :km).where.not(id: @user.groups.map(&:id))
     @groups = [] if @groups.empty?
     render json: @groups and return
   end
