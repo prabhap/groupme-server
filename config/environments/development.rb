4b7bd077dce793a77c6f6b6475858c8a4c12e805
Prabha Pattabiraman <prabhap@thoughtworks.com>
2016-04-11 11:12:40 +0530

    prabha - Changed configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,17 +14,19 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 
 
   config.action_mailer.smtp_settings = {address:'smtp.gmail.com', port:587,
       user_name: 'groupme.noreply@gmail.com',
+      domain: 'gmail.com',
       password: 'likemike1',
-      authentication: 'plain',
-      openssl_verify_mode: 'none'
+      authentication: :login,
+      openssl_verify_mode: 'none',
+      enable_starttls_auto: true
 
   }
   # Print deprecation notices to the Rails logger.
